06a987c36eaa3e4e7252472b4dccc67aacf61b2b
tmygupta <100881820+tmygupta@users.noreply.github.com>
2026-01-27 15:34:55 +0100

    Began writing master generaiton function

---
+++ circuit_simulation/data_generator.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def draw_params(N : int, r_limits=(0,0.8), theta_limits=(0,np.pi/2),
+                     phi_limits=(0,2*np.pi)):
+    """
+    Draw parameters for the quantum optical setup.
+    Parameters
+    ----------
+    N : int, number of samples to draw
+    r_limits : tuple, limits for the squeezing parameter r (inclusive)
+    theta_limits : tuple, limits for the angle theta (inclusive)
+    phi_limits : tuple, limits for the angle phi (exclusive)
+    Returns
+    -------
+    r : np.ndarray, array of shape (N, 9) containing the drawn parameters
+    """
+    
+    rng = np.random.default_rng()
+    r = rng.uniform(r_limits[0], r_limits[1], size=(N, 3))
+    theta = rng.uniform(theta_limits[0], theta_limits[1], size=(N, 3))
+    phi = rng.uniform(phi_limits[0], phi_limits[1], size=(N, 3))
+    
+    return np.hstack([r, theta, phi])
+
+
+SEED = 42
+N_SAMPLES = 1000
+IMAGE_DIM = 64
+PARAM_LIMS = {
+    'r_lims': (0, 0.8),
+    'theta_lims': (0, np.pi / 2),
+    'phi_lims': (0, 2 * np.pi),
+    'n_modes': (0, 3)
+}
+
+params = draw_params(N_SAMPLES, **PARAM_LIMS)
+
