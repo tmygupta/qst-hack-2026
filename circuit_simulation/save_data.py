02a533d224575b6d8fddb45200ffef859047bdef
tmygupta <100881820+tmygupta@users.noreply.github.com>
2026-01-27 14:41:26 +0100

    save_data fx

---
+++ circuit_simulation/save_data.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def save_data(params,images, filename):
+    """
+    Save the parameters and images to an .npz file.
+    Parameters
+    ----------
+    params : np.ndarray, array of shape (N, 9) containing the parameters
+    images : np.ndarray, array of shape (N, H, W) containing the Wigner images
+    filename : str, name of the file to save the data to
+    """
+    np.savez_compressed(filename, params=params, images=images)
